27691aa06c5bcd7e2b22cbfc09a3e974899d5633
Mark Chovava <markchovava@gmail.com>
2025-08-22 15:25:27 +0200

    Clients

--- src/app/_data/sample/WorksData.tsx
+++ src/app/_data/sample/WorksData.tsx
@@ -1,11 +1,11 @@
 
 
 export const WorksData = {
-    title: "Over the last 13 years",
+    title: "Over the last year",
     data: [
-        {id:1, title: <>200</>, desc: "Clients to-date and growing"},
-        {id:2, title: <>82 <small>%</small></>, desc: "Client growth year on year"},
-        {id:3, title: <>75 <small>%</small></>, desc: "Client globalisation efficiency"},
+        {id:1, title: <>120</>, desc: "Clients to-date and growing"},
+        {id:2, title: <>82<small>%</small></>, desc: "Client growth year on year"},
+        {id:3, title: <>75<small>%</small></>, desc: "Client globalisation efficiency"},
     ]
 }
 
